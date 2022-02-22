69dc3c62c6059db6d35a5225c09ea3b990b75390
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-02-22 14:12:07 -0300

    02. TypeScript e compilador

--- app/app.ts
+++ app/app.ts
@@ -1,3 +1,5 @@
-import { Negociacao } from "./models/negociacao.js";
+import {Negociacao} from "./models/negociacao.js";
+
 const negociacao = new Negociacao(new Date(), 10, 100);
+
 console.log(negociacao.volume);
--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,18 +1,26 @@
 export class Negociacao {
+    private _data;
+    private _quantidade;
+    private _valor;
+
     constructor(data, quantidade, valor) {
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
     }
-    get data() {
+
+    get data(){
         return this._data;
     }
+
     get quantidade() {
         return this._quantidade;
     }
+
     get valor() {
         return this._valor;
     }
+
     get volume() {
         return this._quantidade * this._valor;
     }
