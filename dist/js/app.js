e62e4c80142a80aa309ccd9c3af0663683a13889
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-02-22 09:16:13 -0300

    01. Por que usar TypeScript?

---
+++ dist/js/app.js
@@ -0,0 +1,8 @@
+import {Negociacao} from "./models/negociacao.js";
+
+const negociacao = new Negociacao(new Date(), 10, 100);
+
+console.log(negociacao);
+//negociacao.quantidade = 10000;
+console.log(negociacao.data);
+console.log(negociacao.volume);
