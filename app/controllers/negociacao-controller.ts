bb334b96a8db667ac8049790ba345ed87b496c3c
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-02-28 14:31:18 -0300

    04. Avançando na modelagem do dominio

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,34 +1,36 @@
-import { Negociacao } from "../models/negociacao.js";
-
+import { Negociacao } from '../models/negociacao.js';
+import { Negociacoes } from '../models/negociacoes.js';
 export class NegociacaoController {
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
+    private negociacoes = new Negociacoes();
 
     constructor() {
-        this.inputData = document.querySelector("#data");
-        this.inputQuantidade = document.querySelector("#quantidade");
-        this.inputValor = document.querySelector("#valor");
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
     }
 
     adiciona(): void {
         const negociacao = this.criaNegociacao();
-        console.log(negociacao);
+        this.negociacoes.adiciona(negociacao);
+        console.log(this.negociacoes.lista());
         this.limparFormulario();
     }
 
     criaNegociacao(): Negociacao {
-        const exp: RegExp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ","));
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ','));
         const quantidade = parseInt(this.inputQuantidade.value);
         const valor = parseFloat(this.inputValor.value);
         return new Negociacao(date, quantidade, valor);
     }
 
     limparFormulario(): void {
-        this.inputData.value = "";
-        this.inputQuantidade.value = "";
-        this.inputValor.value = "";
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
         this.inputData.focus();
     }
 }
--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -11,9 +11,9 @@ export class Negociacao {
 
     get data(): Date { return this._data; };
 
-    get quantidade(): number { return this._valor; };
+    get quantidade(): number { return this._quantidade; };
 
     get valor(): number { return this._valor; };
 
-    get volume(): number { return this._valor; };
+    get volume(): number { return this._quantidade * this._valor; };
 }
