f390e0bf88dced56382e3c75ac8339feb397a58e
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-02-28 14:31:18 -0300

    04. Avançando na modelagem do dominio

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -1,26 +1,29 @@
-import { Negociacao } from "../models/negociacao.js";
+import { Negociacao } from '../models/negociacao.js';
+import { Negociacoes } from '../models/negociacoes.js';
 export class NegociacaoController {
     constructor() {
-        this.inputData = document.querySelector("#data");
-        this.inputQuantidade = document.querySelector("#quantidade");
-        this.inputValor = document.querySelector("#valor");
+        this.negociacoes = new Negociacoes();
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
     }
     adiciona() {
         const negociacao = this.criaNegociacao();
-        console.log(negociacao);
+        this.negociacoes.adiciona(negociacao);
+        console.log(this.negociacoes.lista());
         this.limparFormulario();
     }
     criaNegociacao() {
         const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ","));
+        const date = new Date(this.inputData.value.replace(exp, ','));
         const quantidade = parseInt(this.inputQuantidade.value);
         const valor = parseFloat(this.inputValor.value);
         return new Negociacao(date, quantidade, valor);
     }
     limparFormulario() {
-        this.inputData.value = "";
-        this.inputQuantidade.value = "";
-        this.inputValor.value = "";
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
         this.inputData.focus();
     }
 }
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -6,10 +6,10 @@ export class Negociacao {
     }
     get data() { return this._data; }
     ;
-    get quantidade() { return this._valor; }
+    get quantidade() { return this._quantidade; }
     ;
     get valor() { return this._valor; }
     ;
-    get volume() { return this._valor; }
+    get volume() { return this._quantidade * this._valor; }
     ;
 }
