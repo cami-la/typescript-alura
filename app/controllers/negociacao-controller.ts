7001df60d9ea2ac17c59c8c4bb77d6c07e8ca559
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2022-03-04 20:12:42 -0300

    03. Visibilidade de metodos e Enumeration
    Signed-off-by: Camila Cavalcante <camiladsantoscavalcante@gmail.com>

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { DiasDaSemana } from '../enums/dias-da-semana.js';
 import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
 import { MensagemView } from '../views/mensagem-view.js';
@@ -17,16 +18,23 @@ export class NegociacaoController {
         this.negociacoesView.update(this.negociacoes);
     }
 
-    adiciona(): void {
+    public adiciona(): void {
         const negociacao = this.criaNegociacao();
+        if(!this.ehDiaUtil(negociacao.data)){
+            this.mensagemView.update("Apenas negociações em dias úteis são aceitas");
+            return;
+        }
         this.negociacoes.adiciona(negociacao);
-        this.negociacoesView.update(this.negociacoes);
-        this.mensagemView.update("Negociação adicionada com sucesso!");
-        console.log(this.negociacoes.lista());
         this.limparFormulario();
+        this.atualizaView();
     }
 
-    criaNegociacao(): Negociacao {
+    private ehDiaUtil(data: Date): Boolean { 
+        return data.getDay() > DiasDaSemana.DOMINGO 
+        && data.getDay() < DiasDaSemana.SABADO;
+    }
+
+    private criaNegociacao(): Negociacao {
         const exp = /-/g;
         const date = new Date(this.inputData.value.replace(exp, ','));
         const quantidade = parseInt(this.inputQuantidade.value);
@@ -34,10 +42,15 @@ export class NegociacaoController {
         return new Negociacao(date, quantidade, valor);
     }
 
-    limparFormulario(): void {
+    private limparFormulario(): void {
         this.inputData.value = '';
         this.inputQuantidade.value = '';
         this.inputValor.value = '';
         this.inputData.focus();
+    }
+
+    private atualizaView():void{
+        this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update("Negociação adicionada com sucesso!");
     }
 }
